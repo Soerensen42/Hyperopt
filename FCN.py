5a6d2dfd668446abe58cb7af22575236dee43ea5
Nils <64003466+Soerensen42@users.noreply.github.com>
2020-05-05 22:08:29 +0200

    Update FCN.py

--- FCN.py
+++ FCN.py
@@ -12,27 +12,46 @@ import pandas
 from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 
-def Load():
-  store_train = pandas.HDFStore('path/train.h5')
-  df_train = store_train.select("table", stop = 2000)  
-  store_train.close()
-  df_train_labels = df_train["is_signal_new"].to_numpy()
+from params import *
 
-  store_val = pandas.HDFStore('path/val.h5')
-  df_val = store_val.select("table", stop = 2000)
+def Load():
+  #loading in the 3 needed data sets
+  store_train= pandas.HDFStore('{data}/train.h5'.format(Parameters["data_location"]))
+  #if quick is set to 1: only read first 2000 entries (great for debugging/testing)
+  if Parameters["Quick"]==1: 
+      df_train = store_train.select("table", stop = 2000)  
+  else:
+      df_train = store_train.select("table")
+  #closing the file    
+  store_train.close()
+  #saving labels for accuracy calculation
+  df_train_labels = df_train["is_signal_new"].to_numpy()
+  
+  store_val = pandas.HDFStore('{data}/val.h5'.format(Parameters["data_location"]))
+  if Parameters["Quick"]==1:
+      df_val = store_val.select("table", stop = 2000)
+  else:
+      df_val = store_val.select("table")
   store_val.close()
   df_val_labels = df_val["is_signal_new"].to_numpy()
 
-  store_test = pandas.HDFStore('path/test.h5')
-  df_test = store_test.select("table", stop = 2000)   
+  store_test = pandas.HDFStore('{data}/test.h5'.format(Parameters["data_location"]))
+  if Parameters["Quick"]==1:
+      df_test = store_test.select("table", stop = 2000) 
+  else:
+      df_test = store_test.select("table")
   store_test.close()
-
+  
+  #seperating signal and background
   df_signal = df_train[df_train["is_signal_new"]==1]
   df_background = df_train[df_train["is_signal_new"]==0]
-
+  
+  #determining amound of signal and background
   df_signal_sum = df_signal.sum(axis = 0, skipna = True)/len(df_signal.index)
   df_background_sum = df_background.sum(axis = 0, skipna = True)/len(df_background.index)
 
+  #only using the first 20 values since everything past that would be too little to influence the network 
+  # => removing unneccessary calculation of empty data
   cols = [c.format(i) for i in range(20) for c in ["E_{0}",  "PX_{0}",  "PY_{0}",  "PZ_{0}"]]
 
   vectors_val = df_val[cols].to_numpy()
@@ -46,22 +65,18 @@ def Load():
 class FCN(nn.Module):
     def __init__(self):
         super(FCN, self).__init__()
-                
+        
+        #setting the FCN Layers, 
         self.fc1 = nn.Linear(80,l1)
         self.fc2 = nn.Linear(l1,l2)        
         self.fc3 = nn.Linear(l2,l3)           
-        self.fc4 = nn.Linear(k3,k4)         
-        self.fc5 = nn.Linear(k4,k5)
-        self.fc6 = nn.Linear(k5,2)
+        self.fc4 = nn.Linear(l3,l4)         
+        self.fc5 = nn.Linear(l4,l5)
+        self.fc6 = nn.Linear(l5,2)
         
         
     def forward(self, x): 
-        #Running through all used Layers and fitting them in with a relu function
-        for k in range(1,5):
-            if r{}.format(k) == 1:
-                x = F.relu(self.fc{}.format(k)(x))
-        #old version of this
-        '''        
+      	# Activating the amound of Layers the variable nLayers dictates with a relu function
         if r1 == 1:
             x = F.relu(self.fc1(x))
         if r2 == 1:    
@@ -72,8 +87,8 @@ class FCN(nn.Module):
             x = F.relu(self.fc4(x))
         if r5 == 1:   
             x = F.relu(self.fc5(x))
-        '''
-        #Using a softmax on the last layer to fit it between 0...1
+            
+        #Using a softmax on the last layer to fit result between 0 and 1
         x = F.softmax(self.fc6(x))
         
         return x
@@ -92,15 +107,7 @@ def Iteration(nLayers, batch_size, learning_rate, l1,l2,l3,l4,l5):
     l5=int(l5)
     r1=r2=r3=r4=r5=1
     
-    #only using as many layers as nLayers suggests and setting l5 to connect to the last used layer 
-    for i in range (1,4):
-        if nLayers == {}.format(i):
-          for a in range (5-i,5):
-              r{}.format(i)=0
-          l5 = l{}.format(i)    
-          
-    #old version of this      
-    '''
+    #only using as many layers as nLayers suggests and setting l5 to connect to the last used layer      
     if nLayers == 1:
         r2=r3=r4=r5=0
         l5=l1
@@ -112,10 +119,9 @@ def Iteration(nLayers, batch_size, learning_rate, l1,l2,l3,l4,l5):
         l5=l3
     if nLayers == 4:
         r5=0 
-        l5=l4 
-    '''    
+        l5=l4    
     
-    #Handling the model  over to the GPU
+    #Handing the model  over to the GPU
     model = FCN().to(device)
     
     #Setting up x and y values
@@ -124,15 +130,19 @@ def Iteration(nLayers, batch_size, learning_rate, l1,l2,l3,l4,l5):
     x_val = vectors_val    
     y_val = vectors_val_labels
     
-    #defining what optimizer to use (standart is Adam)
+    #defining what optimizer to use (standard is Adam)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
+    
     train_examples = x_train.shape[0]
+    
+    #grouping the data in bunches to not overload the GPU 
     n_batches = int(train_examples/batch_size)
 
     for ep in range(n_epochs):
-
+        
+        #start of one epoch
         idx = np.arange(x_train.shape[0])
+        #randomizing the array so the Network doesnt just count the numbers
         np.random.shuffle(idx)
         
         x_train = x_train[idx]
@@ -142,27 +152,34 @@ def Iteration(nLayers, batch_size, learning_rate, l1,l2,l3,l4,l5):
         
             optimizer.zero_grad()
             
+            #setting the batch size range
             i_start = int(i*batch_size)
             i_stop  = int((i+1)*batch_size)
-           
+            
+            #setting up the datasets
             x = torch.tensor(x_train[i_start:i_stop],dtype=torch.float).to(device)
             y = torch.tensor(y_train[i_start:i_stop],dtype=torch.long).to(device)
-
+            
+            #running everything through the model
             net_out = model(x)
-        
+            
+            #calcualting the loss
             loss = criterion(net_out,y)
-  
+            
+            #creating new model (learning)
             loss.backward()
-        
             optimizer.step()
         
-        
+    #using a different dataset to get accuracy     
     x_test = df_test[cols].to_numpy()
     y_test = df_test["is_signal_new"].to_numpy()
     
+    #comparing the guessed values to the real ones
     y_pred_test_model = model(torch.tensor(x_test,dtype=torch.float).to(device)).detach().cpu()
     y_pred_test = nn.Softmax(dim=1)(y_pred_test_model).numpy()
     test_acc = sum(y_test == np.argmax(y_pred_test,axis=1)) / y_test.shape[0]
+    #saving the test accuracy to pass on
     test_accs.append(test_acc)
     
+    #returning as 1-accuracy as hyperopt tries to minimize
     return(1-max(test_accs)) 
