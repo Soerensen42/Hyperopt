1bd8cd5c16bc6d39f9c5887675440ede9cb1cfa5
Nils <64003466+Soerensen42@users.noreply.github.com>
2020-05-07 14:19:46 +0200

    Update params.py

--- params.py
+++ params.py
@@ -9,10 +9,10 @@ def Set_params():
     Parameters["space"]= [hp.quniform('nLayers', 1, 5,1), #Amount Layers the FCN uses.Number of actual layers is nLayers + 1
                           hp.loguniform('batch_size', np.log(2), np.log(2000 ), #The Size of the "Batches" that are ran through the newtork at the same time
                           hp.loguniform('learning_rate', np.log(0.0001), np.log(1)),#how fast the Network learns aka how fast it accepts new ideas and purposely makes mistakes to possibly make a new discovery
-                          hp.quniform('nodes', 1, 256,8)#Amount of nodes in each Layer
+                          hp.quniform('Nodes', 1, 256,8)#Amount of nodes in each Layer
                           ]
     Parameters["Quick"]=1 #Activating(1) or deactivating(0) Quick mode. This will Train etc. only with 2000 Events
                           #recommended for setup and debugging, very fast              
     Parameters["data_location"]="./data"  #Path where all 3 files are saved       
     Parameters["Epochs_FCN"]=10  #Amound of Epochs the FCN Trains for
-#    Parameters["Iterations"]= 100 #Amount of total iterations the Optimization progress makes 
+    Parameters["Iterations"]= 100 #Amount of total iterations the Optimization progress makes, set to 1000 if total number is irrelevant 
