5432176f3a2a9947af462027a5fd2d79ad5b84f1
Nils <64003466+Soerensen42@users.noreply.github.com>
2020-05-07 16:46:34 +0200

    Update params.py

--- params.py
+++ params.py
@@ -1,5 +1,6 @@
 import numpy as np
-def Set_params():
+from hyperopt import hp
+def set_params():
     Parameters={}
     #defines the Searchspace hyperopt is looking through
     #hp.uniform('name',a,b) draws a value soemwhere between a and b
@@ -7,10 +8,10 @@ def Set_params():
     #hp.loguniform('name',np.log(a),np.log(b)) draws logarithmic between values a and b, but is very heavy 
     #tending to a (since its a logarithmic function) !Setting a as 0 will get you an error!
     Parameters["space"]= [hp.quniform('nLayers', 0, 5,1), #Amount Layers the FCN uses.Number of actual layers is nLayers + 1
-                          hp.loguniform('batch_size', np.log(2), np.log(2000 ), #The Size of the "Batches" that are ran through the newtork at the same time
+                          hp.loguniform('batch_size', np.log(2), np.log(2000 )), #The Size of the "Batches" that are ran through the newtork at the same time
                           hp.loguniform('learning_rate', np.log(0.0001), np.log(1)),#how fast the Network learns aka how fast it accepts new ideas and purposely makes mistakes to possibly make a new discovery
-                          hp.quniform('Nodes', 1, 256,8)#Amount of nodes in each Layer
-                          ]
+                          hp.quniform('Nodes', 1, 256,8)]#Amount of nodes in each Layer
+                          
     Parameters["Quick"]=1 #Activating(1) or deactivating(0) Quick mode. This will Train etc. only with 2000 Events
                           #recommended for setup and debugging, very fast              
     Parameters["data_location"]="./data"  #Path where all 3 files are saved       
