4835e7ea6c2aa8272e53b0f7898ac06436323866
Nils <64003466+Soerensen42@users.noreply.github.com>
2020-05-08 16:22:38 +0200

    Update Core.py

--- Core.py
+++ Core.py
@@ -6,32 +6,28 @@ import os
 import numpy as np
 
 ####################Input the parameters#########################
-def set_params():
-    #defines the Searchspace hyperopt is looking through
-    #hp.uniform('name',a,b) draws a value soemwhere between a and b
-    #hp.quniform('name',a,b,c) draws from [a,1+c,a+2c,...,b-c,b] This has to fit, so a+Xc=b must be true
-    #hp.loguniform('name',np.log(a),np.log(b)) draws logarithmic between values a and b, but is very heavy 
-    #tending to a (since its a logarithmic function) !Setting a as 0 will get you an error!
-    Parameters["space"]= [hp.quniform('nLayers', 0, 5,1), #Amount Layers the FCN uses.Number of actual layers is nLayers + 1
+Parameters={}
+#defines the Searchspace hyperopt is looking through
+#hp.uniform('name',a,b) draws a value soemwhere between a and b
+#hp.quniform('name',a,b,c) draws from [a,1+c,a+2c,...,b-c,b] This has to fit, so a+Xc=b must be true
+#hp.loguniform('name',np.log(a),np.log(b)) draws logarithmic between values a and b, but is very heavy 
+#tending to a (since its a logarithmic function) !Setting a as 0 will get you an error!
+Parameters["space"]= [hp.quniform('nLayers', 0, 5,1), #Amount Layers the FCN uses.Number of actual layers is nLayers + 1
                           hp.loguniform('batch_size', np.log(2), np.log(2000 )), #The Size of the "Batches" that are ran through the newtork at the same time
                           hp.loguniform('learning_rate', np.log(1), np.log(0.0001)),#how fast the Network learns aka how fast it accepts new ideas and purposely makes mistakes to possibly make a new discovery
                           hp.quniform('Nodes', 1, 256,8)]#Amount of nodes in each Layer
                           
-    Parameters["Quick"]=1 #Activating(1) or deactivating(0) Quick mode. This will Train etc. only with 2000 Events
-                          #recommended for setup and debugging, very fast              
-    Parameters["data_location"]="./data"  #Path where all 3 files are saved       
-    Parameters["Epochs_FCN"]=10  #Amound of Epochs the FCN Trains for
-    Parameters["Iterations"]= 100 #Amount of total iterations the Optimization progress makes, set to 1000 if total number is irrelevant 
-
+Parameters["Quick"]=1 #Activating(1) or deactivating(0) Quick mode. This will Train etc. only with 2000 Events
+                      #recommended for setup and debugging, very fast              
+Parameters["data_location"]="./data"  #Path where all 3 files are saved       
+Parameters["Epochs_FCN"]=10  #Amound of Epochs the FCN Trains for
+Parameters["Iterations"]= 100 #Amount of total iterations the Optimization progress makes, set to 1000 if total number is irrelevant 
 ####################Start of the Script##########################
-
-Parameters = {}
-set_params()
 
 def objective(args):
     #include Crashes in search history (for things like hardware limitations)
     try:
-        res = Iteration(*args)
+        res = Iteration(*args,Parameters)
         return(res) 
     except:
         #forwarding worst possible result to hyperopt when training crashes (since im optimizing for 1-Accuracy its 1)
